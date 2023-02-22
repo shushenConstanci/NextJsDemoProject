96d0d4dfaa6ba6573095f01bb466c7512c22668f
shushenConstanci <shushen@constanci.com>
2023-02-22 13:11:30 -0800

    First commit for Next JS project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+const Heading = styled.h1`
+    font-size: 50px;
+    color: #333;
+`
+
+function Home() {
+  return <div>Welcome to Next.js!</div>
+}
+export default Home
